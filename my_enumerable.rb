4c9eb3fa023633cd2d515f403646ebfaf4bef572
christian <christianbiringanine22@gmail.com>
2022-07-15 15:23:48 +0200

    add the all and any method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,2 +1,20 @@
 module MyEnumerable
+  def all?
+    array = []
+    @list.each do |item|
+      array.push(item) if yield item
+    end
+    array.length === @list.length
+  end
+
+  def any?
+    array = []
+    @list.each do |item|
+      array.push(item) if yield item
+    end
+    !array.empty?
+  end
+
+  def filter
+    array = []
 end
