cc8b5988c06f8eaf9664a3105c46574d75342652
christian <christianbiringanine22@gmail.com>
2022-07-15 15:37:09 +0200

    Add the filter methode

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,20 +1,19 @@
 module MyEnumerable
-  def all?
-    array = []
-    @list.each do |item|
-      array.push(item) if yield item
-    end
-    array.length === @list.length
+  def all?(&block)
+    result = true
+    each { |item| result = false unless block.call(item) }
+    result
   end
-
-  def any?
-    array = []
-    @list.each do |item|
-      array.push(item) if yield item
-    end
-    !array.empty?
+  def any?(&block)
+    result = false
+    each { |item| result = true if block.call(item) }
+    result
   end
-
-  def filter
-    array = []
+  def filter(&block)
+    result = []
+    each { |item| result << item if block.call(item) }
+    result
+  end
 end
+
+
